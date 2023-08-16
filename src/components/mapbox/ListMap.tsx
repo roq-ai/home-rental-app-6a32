5657e606768629597d7db89325e8aae1ab214226
beshirD <beshirdek@gmail.com>
2023-08-17 01:34:55 +0300

    fix marker out of bound issue

--- src/components/mapbox/ListMap.tsx
+++ src/components/mapbox/ListMap.tsx
@@ -1,10 +1,15 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import { Box } from "@chakra-ui/react";
-
+import { getCenter } from "geolib";
 const ListMap = ({ locations }: any) => {
   const mapContainerRef = useRef(null);
-  const mapRef = useRef(null);
+
+  const coordinates = locations.map((location: any) => ({
+    longitude: location.longitude,
+    latitude: location.latitude,
+  }));
+  const mapCenter = getCenter(coordinates);
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_TOKEN;
@@ -12,58 +17,21 @@ const ListMap = ({ locations }: any) => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [locations[0]?.longitude, locations[0]?.latitude],
-      zoom: 12.5,
+      center: [mapCenter?.latitude, mapCenter?.longitude],
+      zoom: 1,
     });
 
-    mapRef.current = map;
+    locations.map((location: any) =>
+      new mapboxgl.Marker()
+        .setLngLat([location.longitude, location.latitude])
+        .addTo(map)
+    );
 
-    const markers = locations.map((location: any) => {
-      const marker = new mapboxgl.Marker().setLngLat([
-        location.longitude,
-        location.latitude,
-      ]);
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-      marker.addTo(map);
-
-      return marker;
-    });
-
-    return () => {
-      markers.forEach((marker: { remove: () => any }) => marker.remove());
-      map.remove();
-    };
-  }, [locations]);
-
-  useEffect(() => {
-    if (mapRef.current) {
-      const map = mapRef.current;
-
-      locations.forEach((location: { longitude: number; latitude: number }) => {
-        const marker = new mapboxgl.Marker().setLngLat([
-          location.longitude,
-          location.latitude,
-        ]);
-
-        const markerEl = marker.getElement();
-
-        // Calculate the offset based on the marker's size
-        const offsetX = -markerEl.offsetWidth / 2;
-        const offsetY = -markerEl.offsetHeight / 2;
-
-        // Project the LatLng coordinates to pixel coordinates
-        const pos = map.project(marker.getLngLat());
-
-        // Adjust the pixel coordinates based on the offset
-        const adjustedPos = pos.add(new mapboxgl.Point(offsetX, offsetY));
-
-        // Convert the adjusted pixel coordinates back to LatLng
-        const adjustedLatLng = map.unproject(adjustedPos);
-
-        marker.setLngLat(adjustedLatLng);
-      });
-    }
-  }, [locations]);
+    // Clean up on unmount
+    return () => map.remove();
+  });
 
   return (
     <Box ref={mapContainerRef} style={{ width: "auto", height: "500px" }} />
