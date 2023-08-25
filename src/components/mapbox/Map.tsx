c423c82eadd6204e650acccfc4c68f75ab006590
beshirD <beshirdek@gmail.com>
2023-08-25 11:43:43 +0300

    map fix

--- src/components/mapbox/Map.tsx
+++ src/components/mapbox/Map.tsx
@@ -1,106 +1,53 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
-import {
-  Box,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-} from "@chakra-ui/react";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_TOKEN;
 
 const Map = ({
-  // setLocationName,
-  onLocationSelect,
-  setLongitude,
-  setLatitude,
-  latitude,
-  longitude,
-}: any) => {
-  const mapContainerRef = useRef(null);
-  const markerRef = useRef(null);
+    onLocationSelect,
+    setLongitude,
+    setLatitude,
+    latitude,
+    longitude,
+}:any) => {
+  const [map, setMap] = useState(null);
+
   useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_TOKEN;
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [longitude as any, latitude as any],
-      zoom: longitude || latitude ? 3 : 1,
-    });
-
-    map.on("load", () => {
-      map.resize();
-    });
-
-    const geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-      marker: true,
-      flyTo: false,
-    });
-
-    map.addControl(geocoder, "top-right");
-
-    geocoder.on("result", function (e) {
-      const longitude = String(e.result.geometry.coordinates[0]);
-      const latitude = String(e.result.geometry.coordinates[1]);
-      const name = e.result.place_name;
-      // setLocationName(name)
-      setLongitude(longitude);
-      setLatitude(latitude);
-      onLocationSelect({ name, longitude, latitude });
-      if (!markerRef.current) {
-        markerRef.current = new mapboxgl.Marker({
-          color: "green",
-          draggable: true,
-        })
-          .setLngLat([
-            longitude as unknown as number,
-            latitude as unknown as number,
-          ])
-          .addTo(map);
-      } else {
-        markerRef.current.setLngLat([longitude, latitude]);
-      }
-
-      map.flyTo({
-        center: e.result.geometry.coordinates,
-        essential: true,
-        zoom: 3,
+    const initializeMap = () => {
+      const mapInstance = new mapboxgl.Map({
+        container: "map", // HTML element ID
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [longitude as any, latitude as any],
+        zoom: longitude || latitude ? 6 : 1,
       });
-    });
 
-    return () => {
-      if (markerRef.current) {
-        markerRef.current.remove();
-      }
-      map.remove();
+      const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      });
+      mapInstance.addControl(geocoder);
+
+      setMap(mapInstance);
+      
+      geocoder.on("result", function (e) {
+        const longitude = String(e.result.geometry.coordinates[0]);
+        const latitude = String(e.result.geometry.coordinates[1]);
+        const name = e.result.place_name;
+        setLongitude(longitude);
+        setLatitude(latitude);
+        onLocationSelect({ name, longitude, latitude });
+       
+      });
     };
-  }, [latitude, longitude, onLocationSelect, setLatitude, setLongitude]);
 
-  return (
-    <Box>
-      <Box
-        bg="blue.900"
-        color="white"
-        p={2}
-        fontFamily="monospace"
-        position="absolute"
-        top={0}
-        left={0}
-        m={3}
-        borderRadius="md"
-        boxShadow="0px 2px 4px rgba(0, 0, 0, 0.1)"
-        zIndex={10}
-      >
-        Longitude: {longitude} | Latitude: {latitude}
-      </Box>
-      <Box>
-        <Box ref={mapContainerRef} width="500px" height="300px" />
-      </Box>
-    </Box>
-  );
+    if (!map) {
+      initializeMap();
+    }
+  }, [latitude, longitude, map, onLocationSelect, setLatitude, setLongitude]);
+
+  return <div id="map" style={{ width: "100%", height: "500px" }} />;
 };
 
 export default Map;
